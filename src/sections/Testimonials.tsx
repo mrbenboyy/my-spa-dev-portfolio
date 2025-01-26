167227a77eb09276780cbc85286fc033d01ad63c
mrbenboyyy <mrbenboyy@gmail.com>
2025-01-26 20:27:16 +0100

    Add TestimonialsSection and refactor ProjectsSection with new components for improved structure and styling

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -3,6 +3,9 @@ import memojiAvatar2 from "@/assets/images/memoji-avatar-2.png";
 import memojiAvatar3 from "@/assets/images/memoji-avatar-3.png";
 import memojiAvatar4 from "@/assets/images/memoji-avatar-4.png";
 import memojiAvatar5 from "@/assets/images/memoji-avatar-5.png";
+import Card from "@/components/Card";
+import SectionHeader from "@/components/SectionHeader";
+import Image from "next/image";
 
 const testimonials = [
   {
@@ -38,5 +41,31 @@ const testimonials = [
 ];
 
 export const TestimonialsSection = () => {
-  return <div>Testimonials Section</div>;
+  return <div className="py-16 lg:py-24">
+    <div className="container">
+      <SectionHeader
+        eyebrow="Happy Friends"
+        title="What My Friends Say About Me"
+        description="Don&apos;t just take my word for it. See what my friends have to say about my Work."
+      />
+      <div className="mt-16 lg:mt-24 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] ">
+        <div className="flex flex-none gap-8">
+          {testimonials.map((testimonial, index) => (
+            <Card key={index} className="max-w-xs md:max-w-md md:p-8">
+              <div className="flex gap-4 items-center">
+                <div className="size-14 bg-gray-700 inline-flex rounded-full items-center justify-center flex-shrink-0">
+                  <Image src={testimonial.avatar} alt={testimonial.name} className="max-h-full" />
+                </div>
+                <div>
+                  <div className="font-semibold"> {testimonial.name} </div>
+                  <div className="text-sm text-white/40"> {testimonial.position} </div>
+                </div>
+              </div>
+              <p className="mt-4 md:mt-6 text-sm md:text-base">{testimonial.text}</p>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </div>
+  </div>;
 };
