8e93792debd4879d47e4693ad4a07f715aefdd26
mrbenboyyy <mrbenboyy@gmail.com>
2025-01-28 15:39:59 +0100

    Update HeroSection image size for improved visual presentation

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -54,7 +54,7 @@ export const HeroSection = () => {
         <Image
           src={memojiImage}
           alt="Person peeking from behind laptop"
-          className="size-[100px]"
+          className="size-[110px]"
         />
         <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
           <div className="bg-green-500 size-2.5 rounded-full"></div>
