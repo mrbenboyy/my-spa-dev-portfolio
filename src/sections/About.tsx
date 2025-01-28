80be483c6928c98bc4c5fb62539ba15301d14ecf
mrbenboyyy <mrbenboyy@gmail.com>
2025-01-28 17:39:24 +0100

    Update hobbies display in AboutSection with positioning and new entries for enhanced visual appeal

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -43,27 +43,45 @@ const toolboxItems = [
 const hobbies = [
   {
     title: 'Football',
-    emoji: '⚽️'
+    emoji: '⚽️',
+    left: '5%',
+    top: '5%'
   },
   {
     title: 'Photography',
-    emoji: '📷'
-  },
-  {
-    title: 'Gaming',
-    emoji: '🎮'
+    emoji: '📷',
+    left: '50%',
+    top: '5%'
   },
   {
     title: 'Music',
-    emoji: '🎵'
+    emoji: '🎵',
+    left: '35%',
+    top: '40%'
+  },
+  {
+    title: 'Gaming',
+    emoji: '🎮',
+    left: '10%',
+    top: '35%'
   },
   {
     title: 'Fitness',
-    emoji: '🏋️‍♂️'
+    emoji: '🏋️‍♂️',
+    left: '70%',
+    top: '45%'
+  },
+  {
+    title: 'Tennis',
+    emoji: '🎾',
+    left: '5%',
+    top: '65%'
   },
   {
     title: 'Reading',
-    emoji: '📚'
+    emoji: '📚',
+    left: '45%',
+    top: '70%'
   },
 ]
 
@@ -97,15 +115,19 @@ export const AboutSection = () => {
           <ToolboxItems items={toolboxItems} className="mt-6" itemsWrapperClassName="-translate-x-1/2" />
         </Card>
 
-        <Card>
+        <Card className="h-[320px] p-0 flex flex-col">
           <CardHeader
             title="Beyond the Code"
             description="Explore my interests and hobbies beyond the digital realm."
+            className="px-6 py-6"
           />
-          <div>
+          <div className="relative flex-1">
             {hobbies.map((hobby, index) => (
-              <div key={index}>
-                <span> {hobby.title} </span>
+              <div key={index} className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute" style={{
+                left: hobby.left,
+                top: hobby.top
+              }}>
+                <span className="font-medium text-gray-950"> {hobby.title} </span>
                 <span> {hobby.emoji} </span>
               </div>
             ))}
