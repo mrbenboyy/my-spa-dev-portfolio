adf01a0da01a193d7a10f3dcba249b699f4785f8
mrbenboyyy <mrbenboyy@gmail.com>
2025-01-28 19:27:24 +0100

    Refactor AboutSection layout for improved responsiveness and visual structure; enhance Card and CardHeader components with better padding and styling

--- src/components/CardHeader.tsx
+++ src/components/CardHeader.tsx
@@ -7,11 +7,11 @@ export const CardHeader = ({ title, description, className }
         description: string;
         className?: string
     }) => {
-    return <div className={twMerge("flex flex-col", className)}>
+    return <div className={twMerge("flex flex-col p-6 md:py-8 md:px-10", className)}>
         <div className="inline-flex items-center gap-2">
             <StarIcon className="size-9 text-emerald-300" />
             <h3 className="font-serif text-3xl"> {title} </h3>
         </div>
-        <p className="text-sm text-white/60 mt-2"> {description} </p>
+        <p className="text-sm lg:text-base max-w-xs text-white/60 mt-2"> {description} </p>
     </div>
 }
