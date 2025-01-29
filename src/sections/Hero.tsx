094d14a0c767db5937caf5c09675b67265fda798
mrbenboyyy <mrbenboyy@gmail.com>
2025-01-29 14:58:35 +0100

    Refactor HeroSection layout by adjusting mask-image style for improved visual consistency

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -7,8 +7,8 @@ import SparkleIcon from '@/assets/icons/sparkle.svg';
 import { HerOrbit } from "@/components/HeroOrbit";
 
 export const HeroSection = () => {
-  return <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)] ">
-    <div className="absolute inset-0">
+  return <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
+    <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
       <div className="absolute inset-0 -z-30 opacity-5" style={{
         backgroundImage: `url(${grainImage.src})`,
       }}>
