c62f25f9cabeebc0786a00b12587ec81d65752b6
mrbenboyyy <mrbenboyy@gmail.com>
2025-01-30 16:45:01 +0100

    Update Contact and Footer sections with external links and improved styling

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -2,7 +2,7 @@ import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
 import grainImage from '@/assets/images/grain.jpg';
 
 export const ContactSection = () => {
-  return <div id="contact" className="py-16 pt-12 lg:py-24 lg:pt-20">
+  return <div id="contact" className="py-16 pt-12 lg:py-24 lg:pt-20 z-10 relative">
     <div className="container">
       <div className="bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center md:text-left relative overflow-hidden z-0">
         <div className="absolute inset-0 opacity-5 -z-10" style={{
@@ -16,10 +16,10 @@ export const ContactSection = () => {
             </p>
           </div>
           <div>
-            <button className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 w-max border border-gray-900">
+            <a href="https://www.linkedin.com/in/benbouanane-abdelhakim-8a41242a8" target="_blank" className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 w-max border border-gray-900">
               <span className="font-semibold">Contact Me</span>
               <ArrowUpRightIcon className="size-4" />
-            </button>
+            </a>
           </div>
         </div>
       </div>
