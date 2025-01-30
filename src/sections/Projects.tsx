db8927caad3b510db911936196a6296abe2a12c0
mrbenboyyy <mrbenboyy@gmail.com>
2025-01-30 22:28:32 +0100

    Add target="_blank" to project links for opening in a new tab

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -76,7 +76,7 @@ export const ProjectsSection = () => {
                   </li>
                 ))}
                 </ul>
-                <a href={project.link}>
+                <a href={project.link} target="_blank">
                   <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
                     <span>View Project</span>
                     <ArrowUpRightIcon className="size-4" />
