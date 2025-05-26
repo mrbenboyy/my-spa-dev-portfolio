5589a115c3a003b968a0dad829dece15f459ddc7
mrbenboyy <mrbenboyy@gmail.com>
2025-05-26 16:15:59 +0100

    Refactor layout component for improved readability and add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,17 @@ import { Inter, Calistoga } from "next/font/google";
 import "./globals.css";
 import { twMerge } from "tailwind-merge";
 
-const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
-const calistoga = Calistoga({ subsets: ['latin'], variable: '--font-serif', weight: ['400'] })
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const calistoga = Calistoga({
+  subsets: ["latin"],
+  variable: "--font-serif",
+  weight: ["400"],
+});
 
 export const metadata: Metadata = {
   title: "Abdelhakim BENBOUANANE Portfolio",
   description: "Modern & Minimalist Abdelhakim Benbouanane Portfolio",
+  icons: { icon: "/assets/me.png" },
 };
 
 export default function RootLayout({
@@ -19,13 +24,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/assets/me.png" />
         <style>{`
           html {
             scroll-behavior: smooth;
           }
         `}</style>
       </head>
-      <body className={twMerge(inter.variable, calistoga.variable, "bg-gray-900 text-white antialiased font-sans")}>
+      <body
+        className={twMerge(
+          inter.variable,
+          calistoga.variable,
+          "bg-gray-900 text-white antialiased font-sans"
+        )}
+      >
         {children}
       </body>
     </html>
